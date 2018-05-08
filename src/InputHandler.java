681ed50ef59d0a6b71555599f8dacfef9c450b02
Adam Mehdi <adam_mehdi@hotmail.ca>
2018-05-08 13:33:20 -0400

    Comments and import statements.

--- src/InputHandler.java
+++ src/InputHandler.java
@@ -19,7 +19,6 @@ import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
 import javafx.scene.input.KeyEvent;
 import javafx.stage.Stage;
-
 import java.util.ArrayList;
 
 public class InputHandler {
--- src/RigidBody.java
+++ src/RigidBody.java
@@ -10,6 +10,18 @@
    	everything
  */
 
+import javafx.animation.AnimationTimer;
+import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
+import javafx.scene.input.KeyEvent;
+import javafx.stage.Stage;
+import java.util.ArrayList;
+
 public class RigidBody {
 
 }
