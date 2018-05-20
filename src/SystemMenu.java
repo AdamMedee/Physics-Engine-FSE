c70527611b7819a574df63a511027540007424c2
leo <leo2001chen@gmail.com>
2018-05-20 00:54:00 -0400

    Half Done. Adding PageView and other options tomorrow

--- src/SystemMenu.java
+++ src/SystemMenu.java
@@ -13,10 +13,20 @@
 
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Insets;
 import javafx.scene.Group;
+import javafx.scene.control.*;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.Scene;
+
+import java.awt.*;
 
 public class SystemMenu {
 
@@ -33,7 +43,7 @@ public class SystemMenu {
 
     // Graphics Initialization
     public Scene systemScene;
-    public Group SystemLayout;
+    public BorderPane SystemLayout = new BorderPane();
     public Button systemB,objectB,back;
 
     private String newScene;
@@ -42,25 +52,176 @@ public class SystemMenu {
     //Constructor for the menu
     public SystemMenu(Environment environment){
         newScene = "SystemMenu";
-        SystemLayout = new Group();
         systemScene = new Scene(SystemLayout,1280,720);
+
+        Pane leftPane = new Pane();
+
+        TabPane tabs = new TabPane();
+        tabs.setTabClosingPolicy(TabPane.TabClosingPolicy.UNAVAILABLE);
+
+
+        GridPane SystemPane = new GridPane();
+        //SystemPane.setPrefSize();
+        Pane ObjectPane = new Pane();
+
+        Tab SystemTab = new Tab();
+        SystemTab.setText("System");
+        SystemTab.setStyle("-fx-pref-width: 125");
+
+
+        Tab ObjectTab = new Tab();
+        ObjectTab.setText("Objects");
+        ObjectTab.setStyle("-fx-pref-width: 125");
+
+        tabs.getTabs().addAll(SystemTab,ObjectTab);
+
+        // ---------SystemPane Initialization
+        SystemPane.setPadding(new Insets(10, 10, 10, 10));
+        SystemPane.setVgap(30);
+        SystemPane.setHgap(10);
+
+
+
+        // ----- First row
+        HBox OriginBox = new HBox();
+
+        Label orgLabel = new Label("Origin");
+
+        orgLabel.setPrefWidth(120);
+        //orgLabel.setMaxWidth(60);
+
+        Label xLabel = new Label("x:");
+        xLabel.setMaxWidth(60);
+        xLabel.setStyle("-fx-font-size: 15");
+
+        TextField xInput = new TextField("0");
+        xInput.setMaxWidth(60);
+
+        Label blkLabel = new Label("");
+        blkLabel.setStyle("-fx-pref-width: 40;");
+        //blkLabel.setPrefWidth(20);
+
+
+        Label yLabel = new Label("y:");
+        yLabel.setMaxWidth(60);
+        yLabel.setStyle("-fx-font-size: 15");
+
+
+        TextField yInput = new TextField("0");
+
+        yInput.setMaxWidth(60);
+        OriginBox.getChildren().addAll(orgLabel,xLabel,xInput,blkLabel,yLabel,yInput);
+
+
+        GridPane.setConstraints(OriginBox,0,0,2,1);
+
+        SystemPane.getChildren().add(OriginBox);
+
+        SystemTab.setContent(SystemPane);
+
+        // --- -Second row
+
+        Label scaleLabel = new Label("Scale:");
+        scaleLabel.setPrefWidth(80);
+
+        TextField scaleInput = new TextField();
+        scaleInput.setPrefWidth(100);
+
+        GridPane.setConstraints(scaleLabel,0,1);
+        GridPane.setConstraints(scaleInput,1,1);
+
+        SystemPane.getChildren().addAll(scaleLabel,scaleInput);
+        //---------------------
+
+        //-----Third Row (Gravity)---------
+
+        Label gravityLabel = new Label("Gravity:");
+        gravityLabel.setPrefWidth(80);
+
+        TextField gravityInput = new TextField();
+        gravityInput.setPrefWidth(100);
+
+        GridPane.setConstraints(gravityLabel,0,2);
+        GridPane.setConstraints(gravityInput,1,2);
+
+        SystemPane.getChildren().addAll(gravityLabel,gravityInput);
+
+        // -------------------------------
+
+        //----- Fourth Row (SideForce)
+
+        Label sideForceLabel = new Label("sideForece:");
+        sideForceLabel.setPrefWidth(80);
+
+        TextField sideForceInput = new TextField();
+        sideForceInput.setPrefWidth(100);
+
+        GridPane.setConstraints(sideForceLabel,0,3);
+        GridPane.setConstraints(sideForceInput,1,3);
+
+        SystemPane.getChildren().addAll(sideForceLabel,sideForceInput);
+
+        //------Fifth Row (SimulationSpeed)-----
+
+        Label speedLabel = new Label("Simulation Speed:");
+
+        TextField speedInput = new TextField();
+        speedInput.setPrefWidth(100);
+
+        GridPane.setConstraints(speedLabel,0,4);
+        GridPane.setConstraints(speedInput,1,4);
+
+        SystemPane.getChildren().addAll(speedLabel,speedInput);
+
+
+//
+//        Label blkLabel2 = new Label();
+//        speedLabel.setPrefWidth(100);
+//
+//        Label speedDisclaimer = new Label("*Lower = More Accurate");
+//        speedDisclaimer.setStyle("-fx-font-size: 5;");
+//
+//        GridPane.setConstraints(blkLabel2,0,5);
+//        GridPane.setConstraints(speedDisclaimer,1,5);
+//
+//        SystemPane.getChildren().addAll(blkLabel2,speedDisclaimer);
+        //-------Final Buttons Row----------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        leftPane.setPrefSize(980,720);
+        tabs.setPrefSize(300,720);
 
         //Environment being run
         this.environment = environment;
-        this.environment.setGroup(SystemLayout);
+        this.environment.setGroup(leftPane);
 
-        //Buttons to change the selected var
+        SystemLayout.setLeft(leftPane);
+
+        //Buttons to change the selected varBGborderBGborder
         systemB = new Button("System");
         systemB.setOnAction(e -> { });
         systemB.setPrefSize(150, 30);
-        systemB.setLayoutX(980);
+        systemB.setLayoutX(0);
         systemB.setLayoutY(0);
         //systemB.setStyle();
 
 
         objectB = new Button("Objects");
         objectB.setPrefSize(150, 30);
-        objectB.setLayoutX(1130);
+        objectB.setLayoutX(150);
         objectB.setLayoutY(0);
 
 
@@ -79,9 +240,12 @@ public class SystemMenu {
         });
 
         // Adds nodes to group
-        SystemLayout.getChildren().add(systemB);
-        SystemLayout.getChildren().add(objectB);
-        SystemLayout.getChildren().add(back);
+//        SystemLayout.getChildren().add(systemB);
+//        SystemLayout.getChildren().add(objectB);
+        leftPane.getChildren().add(back);
+        //rightPane.getChildren().addAll(systemB,objectB);
+
+        SystemLayout.setRight(tabs);
     }
 
 
